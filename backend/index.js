32a9d0c8c91417fd00033976e5556327d535ed08
Aalain-Bizdateup-Dev <aalain@bizdateup.com>
2025-06-04 11:20:10 +0530

    Index.js Added Comments

--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,19 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+// Employee APi
 app.use("/api", createmployeeroute);
-app.use("/api", createdepartmentsroute);
-app.use("/api/get", createdepartmentsroute);
-app.use("/api/get", createdesignationroute);
-app.use("/api/post", createdesignationroute);
 app.use("/api/get", createmployeeroute);
 app.use("/api/patch", createmployeeroute);
+// Departments Api
+app.use("/api/get", createdepartmentsroute);
+app.use("/api", createdepartmentsroute);
+
+// Designation API
+app.use("/api/get", createdesignationroute);
+app.use("/api/post", createdesignationroute);
+
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
