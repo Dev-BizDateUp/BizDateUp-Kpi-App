920fc58d032a51e893f9008fcf61ec9b28a7b57b
Aryan D <aryan.d.dalal@gmail.com>
2025-06-10 13:20:41 +0530

    added edit employee api and error handling in create employee

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const {
     createEmployeeController,
     getEmployeeController,
+    editEmployee,
     changeEmployeeStatus,
 } = require("../controllers/createmployeecontroller");
 
 router.get('/', getEmployeeController)
 router.post('/', createEmployeeController)
-
+router.patch('/id/:emp_id',editEmployee)
 module.exports = router
