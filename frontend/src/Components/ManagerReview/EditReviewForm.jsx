3e84e3787b46e3555399912f529d7161c7c80c1c
Aryan D <aryan.d.dalal@gmail.com>
2025-07-14 15:41:25 +0530

    manager is now drop down

--- frontend/src/Components/ManagerReview/EditReviewForm.jsx
+++ frontend/src/Components/ManagerReview/EditReviewForm.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import { getEmployees, editManagerReview } from "../../Api/Endpoints/endpoints";
+import { GetterContext } from "../Context/NewContext";
 
 function EditReviewForm({ current, onReviewEditted }) {
 
     const { register, handleSubmit, reset } = useForm();
     const [emps, setEmps] = useState([]);
     const [selEmp, setSelEmp] = useState(1);
+    const { MRActions, managers } = useContext(GetterContext);
 
     async function onSubmit(data) {
         let review = data;
@@ -18,7 +20,9 @@ function EditReviewForm({ current, onReviewEditted }) {
         onReviewEditted(res);
         toast.success('Editted manager review');
     }
-
+    useEffect(() => {
+        console.log("Current MR : ", current);
+    }, [])
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -26,27 +30,70 @@ function EditReviewForm({ current, onReviewEditted }) {
                     <label className='font-bold'>
                         Manager Name
                     </label>
-                    <input defaultValue={current.manager_name} placeholder='Enter Manager Name' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('manager_name')} />
+                    <select
+                        defaultValue={current.manager_name}
+                        placeholder='Enter Manager Name'
+                        type='text'
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        {...register('manager_name', { required: "Manager name is required" })}
+                    >
+                        {
+                            managers.map(m => (
+                                <option value={m}>
+                                    {m}
+                                </option>
+                            ))
+                        }
+                    </select>
+                    {/* <input defaultValue={current.manager_name} placeholder='Enter Manager Name' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('manager_name')} /> */}
                     <label className='font-bold'>
                         Review Dates
                     </label>
-                    <input defaultValue={current.review_date} className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' type='datetime-local' {...register('review_date')} />
+                    <input
+                        defaultValue={current.review_date}
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        type='datetime-local' {...register('review_date', { required: "Review date is required" })}
+                    />
                     <label className='font-bold'>
                         Summary of KPIs assesed
                     </label>
-                    <input defaultValue={current.summary_kpi} placeholder='Summary of KPIs Assessed' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('summary_kpi')} />
+                    <input
+                        defaultValue={current.summary_kpi}
+                        placeholder='Summary of KPIs Assessed'
+                        type='text'
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        {...register('summary_kpi')}
+                    />
                     <label className='font-bold'>
                         Strengths Observed
                     </label>
-                    <input defaultValue={current.strengths} placeholder='Enter Strengths Observed' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('strengths')} />
+                    <input
+                        defaultValue={current.strengths}
+                        placeholder='Enter Strengths Observed'
+                        type='text'
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        {...register('strengths')}
+                    />
                     <label className='font-bold'>
                         Areas of improvement
                     </label>
-                    <input defaultValue={current.improvement} placeholder='Enter Areas of Improvement' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('improvement')} />
+                    <input
+                        defaultValue={current.improvement}
+                        placeholder='Enter Areas of Improvement'
+                        type='text'
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        {...register('improvement')}
+                    />
                     <label className='font-bold'>
                         Additional comments
                     </label>
-                    <input defaultValue={current.comment} placeholder='Enter Additional Comments' type='text' className='p-3 border-2 border-[#E1E1E1] rounded-md m-1' {...register('comment')} />
+                    <input
+                        defaultValue={current.comment}
+                        placeholder='Enter Additional Comments'
+                        type='text'
+                        className='p-3 border-2 border-[#E1E1E1] rounded-md m-1'
+                        {...register('comment')}
+                    />
                     <label className='font-bold'>
                         Overall Performance Rating
                     </label>
@@ -67,58 +114,20 @@ function EditReviewForm({ current, onReviewEditted }) {
                         Actions to be taken
                     </label>
                     <div className='flex flex-col gap-1'>
-                        <label className="flex items-center gap-2 m-2">
-                            <input
-                                type="checkbox"
-                                className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
-                                value="no_action"
-                                defaultChecked={current.actions.includes('no_action')}
-                                {...register('actions')}
-                            />
-                            No Action Required
-                        </label>
-                        <label className="flex items-center gap-2 m-2">
-                            <input
-                                type="checkbox"
-                                className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
-                                value="coaching"
-                                defaultChecked={current.actions.includes('coaching')}
-                                {...register('actions')}
-                            />
-                            Coaching/Mentoring
-                        </label>
-                        <label className="flex items-center gap-2 m-2">
-                            <input
-                                type="checkbox"
-                                className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
-                                value="training"
-                                defaultChecked={current.actions.includes('training')}
-                                {...register('actions')}
-                            />
-                            Training required
-                        </label>
-                        <label className="flex items-center gap-2 m-2">
-                            <input
-                                type="checkbox"
-                                className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
-                                value="promotion"
-                                defaultChecked={current.actions.includes('promotion')}
-                                {...register('actions')}
-                            />
-                            Promotion Consideration
-                        </label>
-
-                        <label className="flex items-center gap-2 m-2">
-                            <input
-                                type="checkbox"
-                                className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
-                                value="pip"
-                                defaultChecked={current.actions.includes('pip')}
-                                {...register('actions')}
-                            />
-                            Performance Improvement Plan (PIP)
-                        </label>
-
+                        {
+                            MRActions.map(mr => (
+                                <label className="flex items-center gap-2 m-2">
+                                    <input
+                                        type="checkbox"
+                                        className='w-4 h-4 border-2 border-[#E1E1E1] bg-[#E1E1E1] rounded-none appearance-none checked:bg-blue-600 checked:border-blue-600'
+                                        value={mr.value}
+                                        defaultChecked={current.actions.includes('no_action')}
+                                        {...register('actions')}
+                                    />
+                                    {mr.text}
+                                </label>
+                            ))
+                        }
                     </div>
                     <label className='font-bold'>
                         Goals/Expectations for Next Review Period
