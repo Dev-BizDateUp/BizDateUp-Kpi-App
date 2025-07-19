1e6b36a3df41c30b0b29522389f3068cb8c7e1e0
div18 <114850579+divagr18@users.noreply.github.com>
2025-07-19 19:03:45 +0530

    Deploy branch with fixed only backend structure

--- frontend/src/Components/Context/NewContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-export const GetterContext = createContext({});
-export const SetterContext = createContext({});
-export const AuthContext = createContext({});
