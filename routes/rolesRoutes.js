05c13edd8eb1116ecc56b64cc469c377f93f0b9e
Dev-BizDateUp <support@bizdateup.com>
2025-08-06 12:30:10 +0530

    Deleted Old Files At 6/8/2025

--- routes/rolesRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-    getAllRoles,getRoleInfo
-} = require('../controllers/rolesController.js')
-router.get('/', getAllRoles);
-router.get('/id/:id', getRoleInfo);
-
-module.exports = router;
