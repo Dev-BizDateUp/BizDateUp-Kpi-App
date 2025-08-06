5c57beedcaae4c52389fd1d50a43a637f58e0180
Dev-BizDateUp <support@bizdateup.com>
2025-08-06 12:42:14 +0530

    Made Changes in Index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use('/api/appraisal', authorize, appraisalRoutes);
 //graphs
 app.use('/api/graph', authorize, graphingRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
