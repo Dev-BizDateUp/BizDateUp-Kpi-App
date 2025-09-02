dd9c8d35bc984134b549e462cabb8a538fbd0319
Aalain-Dev <aalainsmst@gmail.com>
2025-09-02 12:15:36 +0530

    Frontend - Added Modal In Given Badges View Btn

--- frontend/src/Components/Badges/GivenBadges.jsx
+++ frontend/src/Components/Badges/GivenBadges.jsx
@@ -1,9 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GetterContext } from "../Context/NewContext";
+import BadgesModal from "./BadgesModal";
 
 const GivenBadges = () => {
   const { empallbadges } = useContext(GetterContext);
-
+  const [modal, setmodal] = useState(false)
+  const [badges, setbadge] = useState(null)
+  const openModal = (badge) => {
+    setbadge(badge)
+    setmodal(true)
+  }
+  const closeModal = () => {
+    setmodal(false)
+  }
+  
   return (
     <>
       <div className="grid grid-cols-4 gap-5">
@@ -20,13 +30,13 @@ const GivenBadges = () => {
               >
                 <div className="flex flex-col justify-between items-center gap-5">
                   <p className="text-white text-2xl font-bold">{badge.employees_badges_receiver_idToemployees.name}</p>
-                  <p className="text-white text-lg text-center"  style={{
-        width: "150px",         // limit container width
-        whiteSpace: "nowrap",   // keep text in one line
-        overflow: "hidden",     // hide extra text
-        textOverflow: "ellipsis" // show "..."
-      }}>{badge.comment}</p>
-                  <button className="text-black bg-white p-2 font-bold text-xl w-[100px] cursor-pointer rounded-xl">
+                  <p className="text-white text-lg text-center" style={{
+                    width: "150px",         // limit container width
+                    whiteSpace: "nowrap",   // keep text in one line
+                    overflow: "hidden",     // hide extra text
+                    textOverflow: "ellipsis" // show "..."
+                  }}>{badge.comment}</p>
+                  <button onClick={() => openModal(badge)} className="text-black bg-white p-2 font-bold text-xl w-[100px] cursor-pointer rounded-xl">
                     View
                   </button>
                 </div>
@@ -34,10 +44,15 @@ const GivenBadges = () => {
                   {badge.status}
                 </div>
               </div>
+
             ))}
+
           </>
         )}
       </div>
+      {
+        modal ? <BadgesModal badge={badges} closeModal={closeModal} /> : null
+      }
     </>
   );
 };
