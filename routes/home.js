899a568ffcdd299309eb47710e9515a5b2855c65
Dev-BizDateUp <support@bizdateup.com>
2025-09-17 12:36:23 +0530

    Deleted Old Code For Deployment

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-// const { route } = require("./createmployeeroute");
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    return res.status(200).send('Hello world!')
-})
-
-module.exports = router;
