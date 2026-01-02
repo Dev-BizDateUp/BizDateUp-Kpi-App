183637e567eef177898d496afe4226bf35c379e5
Dev-BizDateUp <support@bizdateup.com>
2026-01-02 11:48:45 +0530

    Deleted All the code

--- routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginWithUsername, loginWithGoogle,signUp } = require('../controllers/loginController');
-
-router.post('/email', loginWithUsername);
-router.post('/google', loginWithGoogle);
-router.post('/signup', signUp);
-
-module.exports = router;
