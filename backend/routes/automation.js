0c2409c66d182d4382ebbd01a431ba4a2b2de4b9
Dev-BizDateUp <support@bizdateup.com>
2026-01-07 16:58:49 +0530

    Backend - Created Controller that fetchs group record for  manager

--- backend/routes/automation.js
+++ backend/routes/automation.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { daily_entries, get_daily_entries } = require("../controllers/Automation/AutomationController");
+const { daily_entries, get_daily_entries, get_weekly_entries_for_manager } = require("../controllers/Automation/AutomationController");
 const router = express.Router();
 
 router.post("/daily_entries",daily_entries);
 router.get("/get_indiviual_entries",get_daily_entries);
+router.get("/get_weekly_entries_for_manager/:emp_id/:month/:year",get_weekly_entries_for_manager);
 module.exports = router;
